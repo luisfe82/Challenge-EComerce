11aee0f05b79c4806b9d8b845d27adec33f08a9b
Luisfe <luisfediana@gmail.com>
2022-11-02 11:45:50 -0600

    completado 2parte cierre

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,6 @@ import { valida } from "./validaciones.js";
 const inputs = document.querySelectorAll("input");
 const textareas = document.querySelectorAll("textarea");
 
-
-
 inputs.forEach((input) => {
   input.addEventListener("blur", (input) => {
     valida(input.target);
--- assets/js/cierre.js
+++ assets/js/cierre.js
@@ -2,15 +2,13 @@ const cierreSession = document.querySelector("[data-cierre]");
 const cierreSession2 = document.querySelector("[data-cierre2]");
 const cierreSession3 = document.querySelector("[data-cierre3]");
 
-
 function cerrar() {
     var respuesta = confirm("Estas Seguro que deseas salir del administrador");
     if (respuesta) {
-        localStorage.clear();
+        localStorage.removeItem('usuario');
         window.location.href = "../../index.html";
     }
 
-    
 }
 
 cierreSession.addEventListener("click", cerrar);
