167c0710b725f7e427290bdfbb7bfebea3e883f7
Luisfe <luisfediana@gmail.com>
2022-11-09 18:34:52 -0600

    completado mensaje error no hay resultado busqueda

--- controllers/search.js
+++ controllers/search.js
@@ -4,12 +4,13 @@ import { lineaNuevaProducto } from "./producto.controller.js";
 const obtenerInformacion = () => {
   const identificador = new URL(window.location);
   const id = identificador.searchParams.get("id");
+  var vacio = true;
 
   const palabra = document.querySelector("[data-palabra]");
   palabra.innerText = id;
 
   if (id === null) {
-    window.location.href = "/screens/error.html";
+    contenido.innerHTML = '<div class="content--noresult"><div class="content--noresult__caja"><h2 class="content--noresult__titulo">No se encontraron Resultados</h2></div></div>';
   }
 
   const contenido = document.querySelector("[data-contenido]");
@@ -20,10 +21,14 @@ const obtenerInformacion = () => {
       if (almacen.nombre.match(regex)) {
         const nuevaLinea = lineaNuevaProducto(almacen.id, almacen.url, almacen.nombre, almacen.precio, "content");
         contenido.appendChild(nuevaLinea);
+        vacio = false;
       }
     });
+    if (vacio == true) {
+      contenido.innerHTML = '<div class="content--noresult"><div class="content--noresult__caja"><h2 class="content--noresult__titulo">No se encontraron Resultados</h2></div></div>';
+    }
   }).catch(() => alert("hubo un error"));
-
+  
 };
 
 obtenerInformacion();
